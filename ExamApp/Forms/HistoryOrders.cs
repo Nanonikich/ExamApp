32902e69855b3862558c6ddb27a2d5c95dc6854f
iviramor <iviramor.di@gmail.com>
2021-05-09 04:38:28 +0300

    Проверка на админа в UpdateTable() в истории заказа

--- ExamApp/Forms/HistoryOrders.cs
+++ ExamApp/Forms/HistoryOrders.cs
@@ -320,7 +320,7 @@ namespace ExamApp.Forms
                 dgvOrders.DataSource = TableWithAllOrders;
                 dgvOrders.ReadOnly = true;
             }
-            else if (MainWin.User[10].ToString() == "True")
+            else if (MainWin.User[10].ToString() == "True" && (int)MainWin.User[0] != 25)
             {
                 SetValue((int)MainWin.User[0]);
 
